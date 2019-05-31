51f41bc8ff660977f4d73c094c6899488c43f0ef
choiyoungjae <dudwo1344@gmail.com>
2019-05-31 16:09:23 +0900

    ADD NEXTFIT

---
+++ cau15841-pintos2-qemu-linux/threads/palloc.h
@@ -0,0 +1,28 @@
+#ifndef THREADS_PALLOC_H
+#define THREADS_PALLOC_H
+
+#include <stddef.h>
+
+/* How to allocate pages. */
+enum palloc_flags
+  {
+    PAL_ASSERT = 001,           /* Panic on failure. */
+    PAL_ZERO = 002,             /* Zero page contents. */
+    PAL_USER = 004              /* User page. */
+  };
+//어떤 할당 정책을 펼친것인가
+enum polloc_policys
+{
+  FIRSTFIT = 0,
+  NEXTFIT = 1
+  
+}
+
+void palloc_init (size_t user_page_limit);
+void *palloc_get_page (enum palloc_flags);
+void *palloc_get_multiple (enum palloc_flags, size_t page_cnt);
+void palloc_free_page (void *);
+void palloc_free_multiple (void *, size_t page_cnt);
+
+
+#endif /* threads/palloc.h */
